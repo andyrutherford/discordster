cc183675d30f6487a06e2dd86a8b79fbca181581
Andy <andrew.rutherford.6@gmail.com>
2020-08-25 10:03:06 +0700

    create kick command

--- src/bot.js
+++ src/bot.js
@@ -3,15 +3,36 @@ require('dotenv').config();
 const { Client } = require('discord.js');
 
 const client = new Client();
+const PREFIX = '$';
 
 client.on('ready', () => {
   console.log(`${client.user.username} has logged in.`);
 });
 
 client.on('message', (message) => {
+  if (message.author.bot) return;
   console.log(`[${message.author.tag.split('#')[0]}] ${message.content}`);
-  if (message.content === 'hi bot') {
-    message.channel.send('hi');
+  if (message.content.startsWith(PREFIX)) {
+    const [CMD_NAME, ...args] = message.content
+      .trim()
+      .substring(PREFIX.length)
+      .split(/\s+/);
+
+    if (CMD_NAME === 'kick') {
+      if (args.length === 0) return message.reply('Please provide an ID');
+
+      const member = message.guild.members.cache.get(args[0]);
+      if (member) {
+        member
+          .kick()
+          .then((member) => message.channel.send(`${member} was kicked.`))
+          .catch((err) => message.channel.send('I cannot do that.'));
+      } else {
+        message.channel.send('That member was not found.');
+      }
+    } else if (CMD_NAME === 'ban') {
+      message.channel.send('Banned user');
+    }
   }
 });
 
